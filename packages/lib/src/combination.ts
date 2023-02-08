84e2ffe7ce604a9575e1b10cc3dee42a20e0f554
Jimmy Chu <jimmychu0807@gmail.com>
2023-02-08 23:09:38 +0800

    implemented the combination iterator

--- packages/lib/src/combination.ts
+++ packages/lib/src/combination.ts
@@ -4,23 +4,23 @@ class Combination<T> implements Iterable<Set<T | number>> {
   readonly data: T[] | number[];
 
   constructor(n: number, k: number, data?: T[]) {
-    if (n <= 0 || k <= 0) throw new Error('n and k must be positive integer.');
-    if (k > n) throw new Error('n must be greater than or equal to k.');
+    if (n <= 0 || k <= 0) throw new Error('n and k must be positive integer');
+    if (k > n) throw new Error('n must be greater than or equal to k');
     if (Array.isArray(data) && data.length !== n) {
-      throw new Error('`data` should have the same number of entries as `n`.');
+      throw new Error('`data` should have the same number of entries as `n`');
     }
 
     this.n = n;
     this.k = k;
     this.data = data
-      ? data
+      ? [...data]
       : Array(n)
           .fill(0)
           .map((_, idx) => idx);
   }
 
   static choose(n: number, k: number): number {
-    if (n <= 0 || k <= 0) throw new Error('n and k must be positive integer.');
+    if (n <= 0 || k <= 0) throw new Error('n and k must be positive integer');
     if (k > n) return 0;
 
     const iter = Math.min(k, n - k);
@@ -39,16 +39,30 @@ class Combination<T> implements Iterable<Set<T | number>> {
   }
 
   [Symbol.iterator]() {
-    let counter = 0;
+    const current: number[] = new Array(this.k).fill(0).map((_, idx) => idx);
+    let returnedLast = false;
+
     return {
       next: (): IteratorResult<Set<T | number>, undefined> => {
-        if (counter <= this.k) {
-          const value = new Set([this.data[counter]]);
-          counter += 1;
+        const value = new Set(current.map((idx) => this.data[idx]));
+
+        if (current[0] === this.n - this.k) {
+          if (returnedLast) return { value: undefined, done: true };
+          returnedLast = true;
           return { value, done: false };
         }
 
-        return { value: undefined, done: true };
+        for (let i = current.length - 1; i >= 0; i--) {
+          if (current[i] < this.n - (current.length - i)) {
+            current[i] += 1;
+            for (let j = i + 1; j < current.length; j++) {
+              current[j] = current[j - 1] + 1;
+            }
+            break;
+          }
+        }
+
+        return { value, done: false };
       },
     };
   }
