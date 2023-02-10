cf85788213804660cd7d5fb018a992895d99a9b7
Jimmy Chu <jimmychu0807@gmail.com>
2023-02-10 17:17:43 +0800

    Added chakra-ui

--- apps/main-app/src/App.tsx
+++ apps/main-app/src/App.tsx
@@ -4,8 +4,7 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
-  return (
+  return <>
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -15,7 +14,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>My App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +27,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  </>
 }
 
 export default App
