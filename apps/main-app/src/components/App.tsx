423d16c127a1fa24abad5e66f66283132f459567
Jimmy Chu <jimmychu0807@gmail.com>
2023-02-11 17:52:03 +0800

    updated

--- apps/main-app/src/components/App.tsx
+++ apps/main-app/src/components/App.tsx
@@ -1,11 +1,33 @@
 import { Flex, Heading, SimpleGrid, VStack } from '@chakra-ui/react';
+import { useState } from 'react';
 
 import Readme from './Readme';
 import InputPane from './InputPane';
 import OutputPane from './OutputPane';
 import './App.css';
 
+export interface InputParams {
+  items: number | string[];
+  k: number;
+  lOrder: number;
+  action?: 'computeTotal' | 'generateAll' | 'getElement';
+}
+
+export interface Compute {
+  computeTotal: (n: number, k: number) => void;
+}
+
 function App() {
+  const [input, setInput] = useState<InputParams>({ items: 5, k: 3, lOrder: 0, action: undefined });
+  const [output, setOutput] = useState<string>('');
+
+  const compute: Compute = {
+    computeTotal: (n: number, k: number) => {
+      console.log(`n: ${n}, k: ${k}`);
+      setOutput((10).toString());
+    },
+  };
+
   return (
     <>
       <Flex direction="column">
@@ -18,8 +40,8 @@ function App() {
 
           <form>
             <SimpleGrid columns={2} spacing={8}>
-              <InputPane />
-              <OutputPane />
+              <InputPane input={input} setInput={setInput} compute={compute} />
+              <OutputPane output={output} />
             </SimpleGrid>
           </form>
         </VStack>
@@ -28,4 +50,4 @@ function App() {
   );
 }
 
-export default App;
+export { App as default };
--- apps/main-app/src/components/OutputPane.tsx
+++ apps/main-app/src/components/OutputPane.tsx
@@ -1,6 +1,11 @@
 import { Flex, Heading, FormControl, Stack, Textarea, Button } from '@chakra-ui/react';
 
-export default function OutputPane() {
+interface OutputPaneProps {
+  output: string;
+}
+
+export default function OutputPane(props: OutputPaneProps) {
+  const { output } = props;
   return (
     <>
       <Stack direction="column" spacing={4}>
@@ -12,7 +17,7 @@ export default function OutputPane() {
             COPY
           </Button>
           <FormControl>
-            <Textarea rows={25} height="auto" placeholder="Result output" />
+            <Textarea rows={25} height="auto" placeholder="Result output" value={output} />
           </FormControl>
         </Flex>
         <Button variant="outline">Clear result</Button>
